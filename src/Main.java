bf6130b71d6a96252fd527ac63d6952a06ef5d03
KrisztinaFoldi <krisztifoldi11@gmail.com>
2019-09-16 15:24:12 +0200

    b1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
   public static void main(String[] args) {
     greet();
+    count();
 
   }
 
   public static void greet(){
     System.out.println("balint");
 
+  }
+
+  public static void count() {
+    System.out.println("23");
   }
 
 }
